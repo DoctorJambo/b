5395854bebb2d7eec6f8587d0f7c63ab41abd6b1
Vlad <arhipov09090az@gmail.com>
2025-04-16 19:46:00 +0300

    finish of feat-01

--- Main.java
+++ Main.java
@@ -3,6 +3,17 @@ public class Main {
     System.out.println("hello world");
 
     System.out.println(result(4));
+
+    String[] plans = {"have brackfest", "go to the university", "go back home"};
+
+    StringBuilder builder = new StringBuilder();
+    builder.append("List: ");
+
+    for (int i = 0; i < plans.length; i++) {
+      builder.append("\n").append(i + 1 + ") ").append(plans[i]);
+    }
+
+    System.out.println(builder);
   }
 
   public static int result(int value) {
