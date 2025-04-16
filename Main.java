3773c71faa47dd452c4c06a65d051869749bf3ef
Vlad <arhipov09090az@gmail.com>
2025-04-16 19:27:05 +0300

    add feat-01

--- Main.java
+++ Main.java
@@ -1,5 +1,12 @@
 public class Main {
   public static void main(String[] args) {
     System.out.println("hello world");
+
+    System.out.println(result(4));
+  }
+
+  public static int result(int value) {
+    value = value / 2;
+    return value;
   }
 }
